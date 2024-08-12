47d710615722c48435a24a9fe738a20a916e76f1
POBOROJO <parijatbhatt1@gmail.com>
2024-08-13 01:04:04 +0530

    Added: Folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
-
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config({
+    path: ".env"
+});
 
 app.use(express.json());
 
@@ -8,4 +12,4 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
